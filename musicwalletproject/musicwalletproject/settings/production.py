bd7f1f56dea8f200f86e89242116edea9ed3f894
Alexandre Pinto <alexandpinto@gmail.com>
2016-10-19 03:48:11 +0100

    update production settings

--- musicwalletproject/musicwalletproject/settings/production.py
+++ musicwalletproject/musicwalletproject/settings/production.py
@@ -4,9 +4,9 @@ Django settings for musicwalletproject project (Production)
 
 from base import *
 
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']
 
 SECRET_KEY = "q1y1*!a&3v5s-hf7ngbue4(z*@lq*0j!_)q$uw!1#70ot-hn0y"
 
@@ -26,8 +26,8 @@ DATABASES = {
     }
 }
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
